a684dab639939b1ef0190afe1627de2a1fc2798d
James Crutchley <jamescrutchley98@gmail.com>
2023-07-19 10:54:01 +1000

    Add form sections

---
+++ src/components/Form-sections/FormSection.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+
+class FormSection extends Component {
+  render() {
+    const { controlId, label, name, type, placeholder, value, onChange, isInvalid, feedback, required=true } = this.props;
+
+    return (
+      <Form.Group className="mb-1 w-100" controlId={controlId}>
+        <Form.Label>{label}:</Form.Label>
+        <Form.Control
+          required
+          onChange={onChange}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          isInvalid={isInvalid}
+        />
+        <Form.Control.Feedback type="invalid">{feedback}</Form.Control.Feedback>
+      </Form.Group>
+    );
+  }
+}
+
+export default FormSection;
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,60 +3,111 @@ import Button from "react-bootstrap/Button";
 
 import Form from "react-bootstrap/Form";
 
+import CV from "./Cv";
+import FormSection from "./Form-sections/FormSection";
+
 class UserForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      section: 1,
       generateCV: false,
       valid: false,
       inputs: {
         fname: "",
         lname: "",
+        education: [],
       },
     };
   }
 
-  //state should represent current 'section' user is filling out and contents of input fields held in object.
-  //     this.state = {
-  //         section: 1,
-  //         inputs: {
-  //             //1
-  //             general: {
-  //                 valid: true,
-  //                 name: '',
-  //                 dob: '',
-  //                 email: '',
-  //             },
-  //             //2
-  //             education: {
-  //                 valid: true,
-  //                 entry: [{
-  //                     name: '',
-  //                     startDate: '',
-  //                     endDate: '',
-  //                     qualification: '',
-  //                 }]
-  //             },
-  //             //3
-  //             experience: {
-  //                 valid: true,
-  //                 entry: [{
-  //                     name: '',
-  //                     title: '',
-  //                     startDate: '',
-  //                     endDate: '',
-  //             }]
-  //         }
-  //     }
-  // }
+  renderSection() {
+    const { section, inputs } = this.state;
+
+    switch (section) {
+      case 1:
+        return (
+          <div className="formSection w-100">
+            <h2 className="sectionHeader">General Info</h2>
+            <Button className="navButton backButton">←</Button>
+
+            <div className="nameSection">
+            {/* first name */}
+            <FormSection
+              controlId="fname"
+              label="First Name"
+              name="fname"
+              value={inputs.fname}
+              type="text"
+              placeholder="John"
+              onChange={this.handleInputChange}
+              isInvalid={inputs.fname === ""}
+              feedback="Please fill out this field."
+            />
+            {/* last name */}
+            <FormSection
+              controlId="lname"
+              label="Last Name"
+              name="lname"
+              value={inputs.lname}
+              type="text"
+              placeholder="Smith"
+              onChange={this.handleInputChange}
+              isInvalid={inputs.lname === ""}
+              feedback="Please fill out this field."
+            />
+            </div>
+            <Button className="navButton nextButton">→</Button>
+          </div>
+        );
+      case 2:
+        return <div className="formSection w-100">
+            <h2 className="sectionHeader">Education</h2>
+            <FormSection
+              controlId="education"
+              label="Education"
+              name="education"
+              value={inputs.education}
+              type="text"
+              placeholder="Harvard Medical School"
+              onChange={this.handleInputChange}
+              isInvalid={inputs.education === ""}
+              feedback="Please fill out this field."
+            />
+            <Button>Add entry</Button>
+            <Button variant="secondary" className="navButton nextButton">→</Button>
+        </div> 
+      case 3:
+        return <div className="formSection w-100">
+        <h2 className="sectionHeader">Experience</h2>
+        <FormSection
+          controlId="experience"
+          label="Experience"
+          name="experience"experience
+          value={inputs.experience}
+          type="text"
+          placeholder="Microsoft"
+          onChange={this.handleInputChange}
+          isInvalid={inputs.experience === ""}
+          feedback="Please fill out this field."
+        />
+        <Button>Add entry</Button>
+        <Button type="submit" variant={this.valid ? "success" : "secondary"}>
+            Generate
+        </Button>
+    </div> 
+      default:
+        return null;
+    }
+  }
+
 
   handleSectionChange(section) {
     // on clicking prev or next section
   }
 
   handleInputChange = (e) => {
-    // validation needs to occur here? bootstrap?
-    // handling user changing an input field value
+    // should be generic
 
     const { name, value } = e.target;
 
@@ -65,7 +116,14 @@ class UserForm extends Component {
         ...prevState.inputs,
         [name]: value,
       },
+      valid: this.isFormValid({ ...prevState.inputs, [name]: value }),
     }));
+  };
+
+  isFormValid = (inputs) => {
+    // Add your validation logic here
+    const { fname, lname, education } = inputs;
+    return fname !== "" && lname !== "" && education !== "";
   };
 
   handleEntryChange() {
@@ -75,18 +133,25 @@ class UserForm extends Component {
   handleFormSubmit = (e) => {
     e.preventDefault();
 
-    // render 'cv' element - take user down page to view it.
-    // if inputs invalid, display appropriate warnings
-    // q: submit button necessary? why not just enable live editing?
     const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.stopPropagation();
-      this.setState({ valid: false }); // Set valid state to false
-      console.log(this.state.valid);
-    } else {
-      this.setState({valid: true})
-      console.log("inputs are valid");
+
+    // Check individual field validity
+    try {
+        const isEducationValid = form.education.checkValidity();
+        const isFnameValid = form.fname.checkValidity();
+        const isLnameValid = form.lname.checkValidity();
+    
+        if (isFnameValid && isLnameValid && isEducationValid) {
+          this.setState({ valid: true });
+          console.log("Form is valid");
+        } else {
+          this.setState({ valid: false });
+          console.log("Form is invalid");
+        }
+    } catch {
+        console.log('uh oh')
     }
+
   };
 
   handleReset() {
@@ -94,7 +159,7 @@ class UserForm extends Component {
   }
 
   render() {
-    const { valid } = this.state;
+    const { valid, inputs, section } = this.state;
     // current section will determine what's currently rendered.
 
     // return (
@@ -105,47 +170,29 @@ class UserForm extends Component {
     //   </form>
     // );
     return (
-      <Form
-        validated={this.state.valid}
-        noValidate
-        onSubmit={this.handleFormSubmit}
-        className="w-75 d-flex flex-column align-items-center"
-      >
-        <Form.Group className="mb-1 w-50" controlId="fname">
-          <Form.Label>First Name:</Form.Label>
-          <Form.Control
-            required
-            onChange={this.handleInputChange}
-            className="w-100"
-            name="fname"
-            type="text"
-            placeholder="First Name"
-            isInvalid={!this.state.inputs.fname}
-          />
-          <Form.Control.Feedback type="invalid">Please fill out this field.</Form.Control.Feedback>
-        </Form.Group>
+      <div className="w-100 d-flex flex-column align-items-center border-primary">
+        <Form
+          noValidate
+          validated={this.state.valid}
+          onSubmit={this.handleFormSubmit}
+          className="p-3 m-5 myForm"
+        >
+          {this.renderSection()}
 
-        <Form.Group className="mb-1 w-50" controlId="formLastName">
-          <Form.Label>Last Name:</Form.Label>
-          <Form.Control
-            required
-            onChange={this.handleInputChange}
-            name="lname"
-            className="w-100"
-            type="text"
-            placeholder="Last Name"
-            isInvalid={!this.state.inputs.lname}
-          />
-          <Form.Control.Feedback type="invalid">Please fill out this field.</Form.Control.Feedback>
-        </Form.Group>
+        </Form>
 
-        <Button type="submit" className="col-md-2">
-          Submit
-        </Button>
-        <p>{valid ? "Form is valid" : "Form is invalid"}</p>
+        <div id="nav-dots">
+            <div className={section == '1' ? 'filled' : null} data-id="1"></div>
+            <div className={section == '2' ? 'filled' : null} data-id="2"></div>
+            <div className={section == '3' ? 'filled' : null} data-id="3"></div>
+        </div>
 
-      </Form>
-
+        {valid ? (
+          <CV details={this.state.inputs} />
+        ) : (
+          null
+        )}
+      </div>
     );
   }
 }
