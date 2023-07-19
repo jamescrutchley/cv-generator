19342ee568d8ef707e5cbb7df875a34148ceeada
James Crutchley <jamescrutchley98@gmail.com>
2023-07-19 16:12:50 +1000

    add value attribute to formSection

--- src/components/Form-sections/FormSection.js
+++ src/components/Form-sections/FormSection.js
@@ -3,12 +3,12 @@ import Form from "react-bootstrap/Form";
 
 class FormSection extends Component {
   render() {
-    const { controlId, label, name, type, placeholder, value, onChange, isInvalid, feedback, required=true } = this.props;
-
+    const { controlId, label, name, type, placeholder, value, onChange, isInvalid, feedback } = this.props;
     return (
       <Form.Group className="mb-1 w-100" controlId={controlId}>
         <Form.Label>{label}:</Form.Label>
         <Form.Control
+          value={value}
           required
           onChange={onChange}
           name={name}
