2acd49a0b123715140cf2830648ce4060a741fc7
James Crutchley <jamescrutchley98@gmail.com>
2023-07-21 13:44:11 +1000

    Remove comments

--- src/components/Form-sections/List.js
+++ src/components/Form-sections/List.js
@@ -31,54 +31,8 @@ class List extends Component {
         ))}
       </div>
     );
-
-    // should be :
-    // return a unit w/ edit and delete buttons THEN wrap that around:
-    // switch case - general or education or experience for rendering of details.
-    // map unique inputs with uniqids(?) for tracking.
   }
 }
 
 export default List;
 
-// console.log(details);
-
-// if (!details || details.length < 0) {
-//   return (
-//     <div className="list">
-//       {section} list:
-//       <div>Nothing to show.</div>
-//     </div>
-//   );
-// }
-
-// if (section === 'General' && details) {
-//     return (
-//       <div className="list">
-//         <h4>Your details:</h4>
-//         {details.map((item, index) => (
-//             <div className="listUnit" key={index}>
-//                 <p><strong>{item}</strong></p>
-//             </div>
-//         ))}
-//       </div>
-//     );
-//   }
-
-// return (
-//   <div className="list">
-//     <h4>Your {section}:</h4>
-//     {details.map((item, index) => (
-//       <div className="listUnit" key={index}>
-//         <div className="listUnitButtons">
-//         <button className="border-0" onClick={handleUnitRemoval}><Trash/></button>
-//         <button className="border-0" onClick={handleUnitEdit}><Pen/></button>
-//         </div>
-//         <p><strong>{item.educationName || item.experienceName}</strong></p>
-//         <em> {item.qualification || item.role}</em>
-//         <p> {item.startDate || item.expStartDate} - {item.endDate || item.expEndDate}</p>
-//       </div>
-//     ))}
-//   </div>
-// );
-// }
