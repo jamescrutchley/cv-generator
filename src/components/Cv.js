6c3e90c1729fbfad76996dc0b2affa08b1916fc3
James Crutchley <jamescrutchley98@gmail.com>
2023-07-23 14:00:21 +1000

    Initial CV layout

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -5,21 +5,60 @@ class CV extends Component {
     constructor(props) {
         super(props);
         this.divStyle = {
-            width: "210mm",
-            height: "297mm",
-            border: "1px solid black",
-            background: "lightblue",
-            fontSize: "22px",
+            border: "2px solid black",
         };
     }
 
   render() {
-    const { fname, lname } = this.props.details;
+    const { general, education, experience } = this.props;
+    console.log('cv exp: ' + experience.expEndDate)
 
     return <div 
     style={this.divStyle}
     className="cv">
-        <h1 className="name">{fname} {lname}</h1>
+        <div className="cvHeader cvSub">
+           <p className="name">{general.fname} {general.lname}</p>
+           <div className="cvContact">
+                <p>{general.address}</p>
+                <p>{general.email}</p>
+                <p>{general.mobile}</p>
+                <p>{general.homePhone}</p>
+           </div>
+
+        </div>
+        <div className="cvEducation cvSub">
+            <h4>Education:</h4>
+
+        {education.map((item, index) => (
+            <div key={index} className="cvEducationEntry">
+                <p>
+                <h5>{item.educationName}</h5>
+                </p>
+                <em> {item.qualification}</em>
+                <p>
+                {item.startDate} - {item.endDate || 'Ongoing'}
+                </p>
+            </div>
+        ))}
+        </div>
+
+        <div className="cvExperience cvSub">
+            <h4>Experience:</h4>
+            {experience.map((item, index) => (
+            <div key={index} className="cvEducationEntry">
+                <p>
+                <h5>{item.experienceName}</h5>
+                </p>
+                <em> {item.role}</em>
+                <p>
+                {item.expStartDate} - {item.expEndDate || 'Ongoing'}
+                </p>
+                <div className="cvDescriptionBlock">
+                    {item.description}
+                </div>
+            </div>
+        ))}
+        </div>
     </div>;
   }
 }
